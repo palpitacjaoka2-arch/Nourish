ab938924b6d23631b6d46d225540ea8f0057ecba
palpitacjaoka2-arch <palpitacjaoka2@gmail.com>
2026-05-30 18:47:56 +0200

    remove auth

--- src/main.js
+++ src/main.js
@@ -2,6 +2,5 @@ import { initApp } from './components/app.js'
 import './style.css'
 
 const root = document.getElementById('root')
-// Use a fixed local user ID - no login needed
 const FIXED_USER = { id: 'local-user-alswi' }
 initApp(root, FIXED_USER)
