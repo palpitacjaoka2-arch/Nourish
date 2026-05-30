8bb1303f860041f8de254dec037318ecb366e9b6
palpitacjaoka2-arch <palpitacjaoka2@gmail.com>
2026-05-30 19:07:44 +0200

    fix PWA start url

--- vite.config.js
+++ vite.config.js
@@ -8,20 +8,16 @@ export default defineConfig({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.svg', 'apple-touch-icon.png'],
       manifest: {
-        name: 'Nourish',
-        short_name: 'Nourish',
-        description: 'Intermittent fasting, weight & period tracker',
-        theme_color: '#fdf0f0',
-        background_color: '#fdf0f0',
-        display: 'standalone',
-        orientation: 'portrait',
-        scope: '/',
-        start_url: '/',
-        icons: [
-          { src: 'icons/icon-192.png', sizes: '192x192', type: 'image/png' },
-          { src: 'icons/icon-512.png', sizes: '512x512', type: 'image/png' },
-          { src: 'icons/icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' }
-        ]
+  name: 'Nourish',
+  short_name: 'Nourish',
+  description: 'Intermittent fasting, weight & period tracker',
+  theme_color: '#fdf0f0',
+  background_color: '#fdf0f0',
+  display: 'standalone',
+  orientation: 'portrait',
+  scope: '/Nourish/',
+  start_url: '/Nourish/',
+  icons: [...]
       },
       workbox: {
         globPatterns: ['**/*.{js,css,html,svg,png,woff2}'],
